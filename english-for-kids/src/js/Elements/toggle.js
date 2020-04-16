79cfefe6fad297d00c47ee602ac2267934aa5a46
Karyna Lohvina <karinalogvina0@gmail.com>
2020-04-16 20:48:51 +0300

    feat: create game/train switcher

--- english-for-kids/src/js/Elements/toggle.js
+++ english-for-kids/src/js/Elements/toggle.js
@@ -2,13 +2,17 @@ import Component from './component';
 
 export default class Toggle extends Component {
   constructor() {
-    super('div', null, 'custom-control', 'custom-switch', 'mb-3');
-    const toggle = new Component('input', 'customSwitch1', 'custom-control-input');
-    const label = new Component('label', null, 'custom-control-label');
+    super('section', null);
+    const toggle = new Component('input', 'switch');
+    const label = new Component('label', null);
+    const train = new Component('span', null, 'off', 'train', 'game')
+    const play = new Component('span', null, 'on', 'play', 'game');
+    const switcherThing = new Component('i', null, 'toggler')
+    train.setTextContent('Train');
+    play.setTextContent('Play');
     toggle.setAttribute('type', 'checkbox');
-    label.setAttribute('for', 'customSwitch1');
-    label.setTextContent('Train');
-    this.append(toggle);
-    this.append(label);
+    label.setAttribute('for', 'switch');
+    label.append(switcherThing);
+    this.append(toggle, label, play, train);
   }
 }
