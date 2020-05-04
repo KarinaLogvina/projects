0e2ef4b68fe3c8759337e7014d8af4f76c978dc0
Karyna Lohvina <karinalogvina0@gmail.com>
2020-05-05 00:12:58 +0300

    feat: add card creating function

--- movie-search/src/app/helper.js
+++ movie-search/src/app/helper.js
@@ -1,23 +1,38 @@
 
 const createPoster = (url) => {
-  const posterContainer = document.createElement('div').classList.add('movie-poster');
+  const posterContainer = document.createElement('div');
+  posterContainer.classList.add('movie-poster');
   const poster = document.createElement('img');
   poster.setAttribute('src', `${url}`);
   posterContainer.append(poster);
   return posterContainer;
 };
 
-const createCard = (title, poster, year, rait) => {
-  const movieContainer = document.createElement('div').classList.add('mocie-container');
-  const movieTitle = document.createElement('h2').classList.add('movie-title');
+const createCard = (title, poster, year, rate, id) => {
+  const movieContainer = document.createElement('div');
+  movieContainer.classList.add('swiper-slide');
+  const movieTitle = document.createElement('a');
+  movieTitle.setAttribute('href', `https://www.imdb.com/title/${id}/?ref_=nv_sr_srsg_0`);
+  movieTitle.setAttribute('target', '_blank');
+  movieTitle.classList.add('movie-title');
   const moviePoster = createPoster(poster);
-  const movieYear = document.createElement('div').classList.add('movie-year');
-  const movieRate = document.createElement('div').classList.add('movie-rait');
+  const movieYear = document.createElement('div');
+  movieYear.classList.add('movie-year');
+  const movieRate = document.createElement('div');
+  movieRate.classList.add('movie-rate');
   movieTitle.textContent = title;
   movieYear.textContent = year;
-  movieRate.textContent = rait;
+  movieRate.textContent = rate.imdbRating;
   movieContainer.append(movieTitle, moviePoster, movieYear, movieRate);
   return movieContainer;
 };
 
-export { createPoster, createCard };
+function parseToPages(elements, pageSize = 4) {
+  const result = [];
+  while (elements.length) {
+    result.push(elements.splice(0, pageSize));
+  }
+  return result;
+}
+
+export { createPoster, createCard, parseToPages };
