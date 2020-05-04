bbefd8856cbaa3e981f834ed3fb396c87806ebac
Karyna Lohvina <karinalogvina0@gmail.com>
2020-05-05 00:13:22 +0300

    feat: create translation function

--- movie-search/src/app/loader.js
+++ movie-search/src/app/loader.js
@@ -6,8 +6,28 @@ const loadMovieRating = async (imdbID) => {
   return json;
 };
 
+
+const getLang = async (word) => {
+  const url = `https://translate.yandex.net/api/v1.5/tr.json/detect?hint=ru,en&key=trnsl.1.1.20200504T194254Z.eff8a96fef4bd463.bf884cb26c02503749a287801028c6555d90c4ca&text=${word}`;
+  const res = await fetch(url);
+  const json = await res.json();
+  return json.lang;
+};
+
+
+const getTranslation = async (word) => {
+  const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200504T194254Z.eff8a96fef4bd463.bf884cb26c02503749a287801028c6555d90c4ca&text=${word}&lang=ru-en`;
+  const res = await fetch(url);
+  const json = await res.json();
+  return json.text[0];
+};
+
 const loadOmdbQuery = async (searchQuery, page = 1) => {
-  const encoded = encodeURIComponent(searchQuery);
+  let encoded = encodeURIComponent(searchQuery || 'dream');
+  const lang = await getLang(encoded);
+  if (lang === 'ru') {
+    encoded = await getTranslation(encoded);
+  }
   const url = `http://www.omdbapi.com/?s=${encoded}&page=${page}&apikey=${omdbKey}`;
   const res = await fetch(url);
   const json = await res.json();
@@ -25,4 +45,5 @@ const loadOmdbQuery = async (searchQuery, page = 1) => {
   return json.Error;
 };
 
-export default loadOmdbQuery;
+
+export { loadOmdbQuery, getTranslation, getLang };
