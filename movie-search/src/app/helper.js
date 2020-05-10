7cea91db95029e09f2bb6c86fa79b8afd0ab2e46
Karyna Lohvina <karinalogvina0@gmail.com>
2020-05-10 22:21:25 +0300

    fix: optimize createCard function

--- movie-search/src/app/helper.js
+++ movie-search/src/app/helper.js
@@ -12,20 +12,20 @@ const createPoster = (url) => {
   return posterContainer;
 };
 
-const createCard = (title, poster, year, rate, id) => {
-  const movieContainer = document.createElement('div');
-  movieContainer.classList.add('swiper-slide');
+const createTitle = (title, id) => {
   const movieTitle = document.createElement('a');
   movieTitle.setAttribute('href', `https://www.imdb.com/title/${id}/?ref_=nv_sr_srsg_0`);
   movieTitle.setAttribute('target', '_blank');
   movieTitle.classList.add('movie-title');
   movieTitle.setAttribute('title', `${title}`);
+  movieTitle.textContent = title;
+  return movieTitle;
+};
+
+const createRate = (rate) => {
   const star = document.createElement('img');
   star.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRWG8OrTDYMbECoTTNp4qFy1h75DcKBpS8t09nMdPNNnJBprAdB&usqp=CAU');
   star.classList.add('icon');
-  const moviePoster = createPoster(poster);
-  const movieYear = document.createElement('div');
-  movieYear.classList.add('movie-year');
   const movieRate = document.createElement('div');
   if (rate.imdbRating === 'N/A') {
     movieRate.textContent = 'No rating';
@@ -33,9 +33,24 @@ const createCard = (title, poster, year, rate, id) => {
     movieRate.textContent = rate.imdbRating;
   }
   movieRate.classList.add('movie-rate');
-  movieTitle.textContent = title;
   movieRate.prepend(star);
+  return movieRate;
+};
+
+const createYear = (year) => {
+  const movieYear = document.createElement('div');
+  movieYear.classList.add('movie-year');
   movieYear.textContent = year;
+  return movieYear;
+};
+
+const createCard = (title, poster, year, rate, id) => {
+  const movieContainer = document.createElement('div');
+  movieContainer.classList.add('swiper-slide');
+  const movieTitle = createTitle(title, id);
+  const moviePoster = createPoster(poster);
+  const movieYear = createYear(year);
+  const movieRate = createRate(rate);
   movieContainer.append(movieTitle, movieYear, moviePoster, movieRate);
   return movieContainer;
 };
