3225e439c1ced138298a7adc3332f18fc9eb4b74
Karyna Lohvina <karinalogvina0@gmail.com>
2020-05-28 21:14:01 +0300

    fix: control block component

--- fancy-weather/src/Components/controlBlock/controlBlock.jsx
+++ fancy-weather/src/Components/controlBlock/controlBlock.jsx
@@ -1,54 +1,82 @@
-import React from 'react';
-import {useSelector, useStore, useDispatch} from 'react-redux';
-import {getLang} from './selectors';
-import {setUnit} from './actions';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getLang } from './selectors';
+import { setUnit } from './actions';
+import { bindActionCreators } from "redux";
 
-function ControlBox () {
-  const lang = useSelector (getLang);
-  const dispatch = useDispatch ();
-  return (
-    <div className="controlBox">
-      <div className="optionsBox">
-        <button>reload</button>
-        <select name="language" id="lang">
-          <option
-            value="en"
-            selected={lang === 'en'}
-            onClick={() => dispatch (setLanguage ('en'))}
-          >
-            EN
-          </option>
-          <option
-            value="ru"
-            selected={lang === 'ru'}
-            onClick={() => dispatch (setLanguage ('ru'))}
-          >
-            RU
-          </option>
-          <option
-            value="be"
-            selected={lang === 'be'}
-            onClick={() => dispatch (setLanguage ('be'))}
-          >
-            BE
-          </option>
-        </select>
-        <button
-          onClick={() => dispatch (setUnit ('C'))}
-          className="btn_unit__active"
-        >
-          °C
-        </button>
-        <button onClick={() => dispatch (setUnit ('F'))} className="btn_unit">
-          F
-        </button>
-      </div>
-      <div className="search">
-        <input type="text" placeholder="Search city" />
-        <button type="submit">Search</button>
-      </div>
-    </div>
-  );
+
+class ControlBox extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    setLanguage(lang) {
+        return () => this.props.setLanguage(lang);
+    }
+
+    setUnit(unit) {
+        return () => this.props.setUnit(unit);
+    }
+
+    render() {
+        return (
+            <div className="controlBox">
+                <div className="optionsBox">
+                    <button>reload</button>
+                    <select name="language" id="lang">
+                        <option
+                            value="en"
+                            selected={this.props.lang === 'en'}
+                            onClick={this.setLanguage('en')}
+                        >
+                            EN
+                        </option>
+                        <option
+                            value="ru"
+                            selected={this.props.lang === 'ru'}
+                            onClick={this.setLanguage('ru')}
+                        >
+                            RU
+                        </option>
+                        <option
+                            value="be"
+                            selected={this.props.lang === 'be'}
+                            onClick={this.setLanguage('be')}
+                        >
+                            BE
+                        </option>
+                    </select>
+                    <button
+                        onClick={this.setUnit('C')}
+                        className="btn_unit__active"
+                    >
+                        °C
+                    </button>
+                    <button onClick={this.setUnit('F')} className="btn_unit">
+                        F
+                    </button>
+                </div>
+                <div className="search">
+                    <input type="text" placeholder="Search city"/>
+                    <button type="submit">Search</button>
+                </div>
+            </div>
+        );
+    };
 }
 
-export default ControlBox;
+const mapStateToProps = (state) => {
+    return {getLang: getLang(state)}
+};
+
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        setUnit: setUnit,
+    }, dispatch)
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ControlBox);
