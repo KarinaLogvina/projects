7c9a837ad4851d8fc6d3917a408ab919660c7d37
Karyna Lohvina <karinalogvina0@gmail.com>
2020-05-30 21:16:21 +0300

    feat: added app state

--- fancy-weather/src/store.js
+++ fancy-weather/src/store.js
@@ -1,6 +1,13 @@
-import { createStore, combineReducers, applyMiddleware  } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import controls from './Components/controlBlock/reducers';
 import weather from './Components/weather/reducers';
 import thunk from 'redux-thunk';
+import app from './Components/app/reducer'
 
-export default createStore(combineReducers({ controls, weather }), applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+);
+
+export default createStore(combineReducers({ controls, weather, app }), enhancer);
